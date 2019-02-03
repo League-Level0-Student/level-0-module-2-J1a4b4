c45f9615eea7235b1eee99eb8e162d05abec3c11
apaara11 <league@WTS21.attlocal.net>
2019-02-02 17:30:00 -0800

    Next Recipe: Lottery Numbers

--- src/random/Magic8Ball.java
+++ src/random/Magic8Ball.java
@@ -6,18 +6,29 @@ package random;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below….
-
+public static void main(String[] args) {
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+Random randy = new Random();
+int answer = randy.nextInt(4);
 	// 3. Print out this variable
-
+System.out.println(answer);
 	// 4. Get the user to enter a question for the 8 ball
-
+JOptionPane.showInputDialog(null, "Concentrate and ask the Magic 8 Ball a question.");
 	// 5. If the random number is 0
-
+if (answer == 0) {
+JOptionPane.showMessageDialog(null, "Yes.");
+}else if (answer == 1) {
+JOptionPane.showMessageDialog(null, "No.");
+}else if (answer == 2) {
+JOptionPane.showMessageDialog(null, "Maybe you should ask Google.");
+}else {
+JOptionPane.showMessageDialog(null, "Do you expect me to know this?.");
+}
 	// -- tell the user "Yes"
 
 	// 6. If the random number is 1
@@ -31,5 +42,5 @@ public class Magic8Ball {
 	// 8. If the random number is 3
 
 	// -- write your own answer
-
+}
 }
--- src/random/Validation.java
+++ src/random/Validation.java
@@ -9,17 +9,25 @@ import javax.swing.JOptionPane;
 
 public class Validation {
 	public static void main(String[] args) {
-		
-		Random randomMaker = new Random();
-		
-		int randomNumber = randomMaker.nextInt(5);
-		
-		System.out.println(randomNumber);
+		for (int i = 0; i < 10; i++) {
+			Random randomMaker = new Random();
 
-		// 1. Use each value of randomNumber to give the user a random compliment.
+			int randomNumber = randomMaker.nextInt(5);
 
-		// 2. Repeat all the code above 10 times
-		
+			System.out.println(randomNumber);
+
+			// 1. Use each value of randomNumber to give the user a random compliment.
+			if (randomNumber == 1) {
+				JOptionPane.showMessageDialog(null, "You are one of the smartest people I know.");
+			} else if (randomNumber == 2) {
+				JOptionPane.showMessageDialog(null, "You look very good today.");
+			} else if (randomNumber == 3) {
+				JOptionPane.showMessageDialog(null, "You are an extremely nice person.");
+			} else {
+				JOptionPane.showMessageDialog(null, "It is a pleasure to know you.");
+			}
+		} // 2. Repeat all the code above 10 times
+
 		// 3. Find someone to test out your program. They will like it :)
 	}
 }
